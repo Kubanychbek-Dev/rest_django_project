45addb75968fe8ba0ba1155cf1c977489b465c71
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-05-17 19:32:59 +0300

    add create_super_user.py

---
+++ users/management/commands/create_super_user.py
@@ -0,0 +1,44 @@
+from django.core.management import BaseCommand
+from users.models import User, UserRoles
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        admin = User.objects.create(
+            email="user_stark@mail.com",
+            first_name="Tony",
+            last_name="Stark",
+            role=UserRoles.ADMIN,
+            is_staff=True,
+            is_active=True,
+            is_superuser=True,
+        )
+        admin.set_password("stark")
+        admin.save()
+        print("The admin created")
+
+        moderator = User.objects.create(
+            email="moderator@mail.com",
+            first_name="Jane",
+            last_name="Stark",
+            role=UserRoles.MODERATOR,
+            is_staff=True,
+            is_active=True,
+            is_superuser=False,
+        )
+        moderator.set_password("stark")
+        moderator.save()
+        print("The moderator created")
+
+        user = User.objects.create(
+            email="johndoe@mail.com",
+            first_name="John",
+            last_name="Doe",
+            role=UserRoles.USER,
+            is_staff=False,
+            is_active=True,
+            is_superuser=False,
+        )
+        user.set_password("user")
+        user.save()
+        print("The user created")
