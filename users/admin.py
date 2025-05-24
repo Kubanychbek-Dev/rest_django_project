37a80cb2c357f3d5b3372820d9ed7a18773092c3
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-05-24 13:32:32 +0300

    add UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+  list_display = ("id", "last_name", "first_name", "email",)
+  list_filter = ("id", "email",)
+  ordering = ("id",)
