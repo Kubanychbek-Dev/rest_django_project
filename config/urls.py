1c5d2b04e82d135a89ad63582df0fb7d09658dd4
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-05-26 19:36:59 +0300

    add Auto-documentation

--- config/urls.py
+++ config/urls.py
@@ -19,8 +19,31 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+  openapi.Info(
+    title="Animals API",
+    default_version="v1.0",
+    description="Animals API",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="someemail"),
+    license=openapi.License(name="License"),
+  ),
+  public=True,
+  permission_classes=[permissions.AllowAny,],
+)
+
 
 urlpatterns = [
+  # Base urls
     path('admin/', admin.site.urls),
+    # App urls
     path('users/', include('users.urls', namespace='users')),
+    # Documentation urls
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name="schema-redoc"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
