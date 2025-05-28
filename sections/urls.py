b323a17ae265f4f8c2c0f9564a1d38e7f7d646af
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2025-05-28 15:56:58 +0300

    add contents

--- sections/urls.py
+++ sections/urls.py
@@ -2,7 +2,7 @@ from os import path as p
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from sections.apps import SectionsConfig
-from sections.views import SectionListAPIView, SectionCreateAPIView, SectionRetrieveAPIView, SectionUpdateAPIView, SectionDestroyAPIView
+from sections.views import SectionListAPIView, SectionCreateAPIView, SectionRetrieveAPIView, SectionUpdateAPIView, SectionDestroyAPIView, ContentListAPIView, ContentCreateAPIView, ContentRetrieveAPIView, ContentUpdateAPIView, ContentDestroyAPIView
 
 app_name = SectionsConfig.name
 
@@ -20,6 +20,12 @@ urlpatterns = [
   path(p.join(section), SectionListAPIView.as_view(), name="section_list"),
   path(p.join(section, create), SectionCreateAPIView.as_view(), name="section_create"),
   path(p.join(section, int_pk), SectionRetrieveAPIView.as_view(), name="section_detail"),
-  path(p.join(section, update), SectionUpdateAPIView.as_view(), name="section_update"),
-  path(p.join(section, delete), SectionDestroyAPIView.as_view(), name="section_delete"),
+  path(p.join(section, int_pk, update), SectionUpdateAPIView.as_view(), name="section_update"),
+  path(p.join(section, int_pk, delete), SectionDestroyAPIView.as_view(), name="section_delete"),
+
+  path(p.join(content), ContentListAPIView.as_view(), name="content_list"),
+  path(p.join(content, create), ContentCreateAPIView.as_view(), name="content_create"),
+  path(p.join(content, int_pk), ContentRetrieveAPIView.as_view(), name="content_detail"),
+  path(p.join(content, int_pk, update), ContentUpdateAPIView.as_view(), name="content_update"),
+  path(p.join(content, int_pk, delete), ContentDestroyAPIView.as_view(), name="content_delete"),
 ] + router.urls
--- sections/views.py
+++ sections/views.py
@@ -6,18 +6,18 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from sections.models import Section, Content
 from sections.permissions import IsModerator, IsSuperUser
 from sections.serializers.section_serializers import SectionSerializer, SectionListSerializer
-from sections.serializers.content_serializers import ContentSerializer, SectionContentSerializer, SectionContentListSerializer
+from sections.serializers.content_serializers import ContentSerializer, SectionContentSerializer, ContentListSerializer
 from sections.paginators import SectionPaginator, ContentPaginator
 
 
 class SectionListAPIView(ListAPIView):
   serializer_class = SectionListSerializer
   queryset = Section.objects.all()
-  permission_classes = (IsAuthenticated)
+ # permission_classes = (IsAuthenticated)
   pagination_class = SectionPaginator
 
 
-class SectionCreateAPIView(ListAPIView):
+class SectionCreateAPIView(CreateAPIView):
   serializer_class = SectionSerializer
  # permission_classes = (IsAuthenticated, IsModerator | IsSuperUser)
 
@@ -25,7 +25,7 @@ class SectionCreateAPIView(ListAPIView):
 class SectionRetrieveAPIView(RetrieveAPIView):
   serializer_class = SectionSerializer
   queryset = Section.objects.all()
-  permission_classes = (IsAuthenticated,)
+ # permission_classes = (IsAuthenticated,)
 
 
 class SectionUpdateAPIView(UpdateAPIView):
@@ -37,4 +37,34 @@ class SectionUpdateAPIView(UpdateAPIView):
 class SectionDestroyAPIView(DestroyAPIView):
   serializer_class = SectionSerializer
   queryset = Section.objects.all()
+  # permission_classes = (IsAuthenticated, IsSuperUser)
+
+
+class ContentListAPIView(ListAPIView):
+  serializer_class = ContentListSerializer
+  queryset = Content.objects.all()
+  # permission_classes = (IsAuthenticated,)
+  pagination_class = ContentPaginator
+
+
+class ContentCreateAPIView(CreateAPIView):
+  serializer_class = ContentSerializer
+  # permission_classes = (IsAuthenticated, IsModerator | IsSuperUser)
+
+
+class ContentRetrieveAPIView(RetrieveAPIView):
+  serializer_class = ContentSerializer
+  queryset = Content.objects.all()
+  # permission_classes = (IsAuthenticated,)
+
+
+class ContentUpdateAPIView(UpdateAPIView):
+  serializer_class = ContentSerializer
+  queryset = Content.objects.all()
+  permission_classes = (IsAuthenticated, IsModerator | IsSuperUser)
+
+
+class ContentDestroyAPIView(DestroyAPIView):
+  serializer_class = ContentSerializer
+  queryset = Content.objects.all()
   # permission_classes = (IsAuthenticated, IsSuperUser)
